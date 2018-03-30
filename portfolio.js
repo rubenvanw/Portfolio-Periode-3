c48b5853c89d4b6b80e9af7d2086565afbedc62f
ruben <rubenvanwoerkom@gmail.com>
2018-03-30 17:39:39 +0200

    third push

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,12 @@
-$( document ).ready(function() {
-    $(".arrow").click(function() {
+$(document).ready(function () {
+    $(".arrow").click(function () {
         $('html, body').animate({
             scrollTop: $(".containerTwee").offset().top
         }, 1000);
     });
 
-    $(window).scroll(function() {
-        if($(document).scrollTop() > 10) {
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 10) {
             $('#navBar').addClass('navClass');
         }
         else {
@@ -36,3 +36,48 @@ $( document ).ready(function() {
     });
 
 });
+
+var sliderImages = document.querySelectorAll('.slide');
+var arrowLeft = document.querySelector('#arrow-left');
+var arrowRight = document.querySelector('#arrow-right');
+var current = 0;
+
+//Clears all images
+function reset() {
+    for (i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = "none";
+    }
+}
+
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = "flex";
+}
+
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = "flex";
+    current--;
+}
+
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = "flex";
+    current++;
+}
+
+arrowLeft.addEventListener('click', function () {
+    if (current === 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
+});
+
+arrowRight.addEventListener('click', function () {
+   if (current === sliderImages.length - 1){
+       current = -1;
+   }
+   slideRight();
+});
+
+startSlide();
